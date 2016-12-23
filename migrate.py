313a63856fd3fa76bfebfc2588393ef05a427048
Jonathan Xu <jonathanj.xu@yahoo.com>
2016-12-23 17:58:36 -0600

    MySQL config.py implemented

--- migrate.py
+++ migrate.py
@@ -8,6 +8,21 @@ def checkMigrationsFolder():
   if not os.path.isdir('migrations'):
     os.makedirs('migrations')
 
+def getDbConfig(args):
+  if (args.host is not None and args.user is not None and
+    args.password is not None and args.database is not None):
+    return {
+      "host": args.host,
+      "user": args.user,
+      "password": args.password,
+      "database": args.database
+    }
+  elif os.path.exists('config.py'):
+    import config
+    return config.mysql
+  else:
+    print("error: database connection information not specified")
+
 def handleArgs(args):
   command = args.command[0]
   if command == 'generate':
@@ -16,6 +31,8 @@ def handleArgs(args):
     migrateHandler(args)
   elif command == 'rollback':
     rollbackHandler(args)
+  elif command == 'reset':
+    resetHandler(args)
   else:
     print('error: unrecognized command ' + command)
 
@@ -30,16 +47,27 @@ def genHandler(args):
 def migrateHandler(args):
   pass
 
-def rollbackhandler(args):
+def rollbackHandler(args):
+  pass
+
+def resetHandler(args):
   pass
 
 def main():
   parser = argparse.ArgumentParser(description='Manage MySQL migrations')
   parser.add_argument('command', nargs='+')
+  parser.add_argument('--host', '-host')
+  parser.add_argument('--user', '-u')
+  parser.add_argument('--password', '-p')
+  parser.add_argument('--database', '-db')
   parser.add_argument('--version', type=int)
+
   checkMigrationsFolder()
-  handleArgs(parser.parse_args())
-  print(parser.parse_args())
+  args = parser.parse_args()
+  db = getDbConfig(args)
+  handleArgs(args)
+  print(args)
+  print(db)
 
 if __name__ == '__main__':
   main()
